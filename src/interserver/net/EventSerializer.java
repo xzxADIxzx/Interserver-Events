409166105c05f5df4319926226a535c38ac19518
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-02-13 19:23:47 +0300

    add framework messages

--- src/interserver/net/EventSerializer.java
+++ src/interserver/net/EventSerializer.java
@@ -1,5 +1,7 @@
 package interserver.net;
 
+import arc.net.FrameworkMessage;
+import arc.net.FrameworkMessage.*;
 import arc.net.NetSerializer;
 import arc.util.Log;
 import mindustry.io.JsonIO;
@@ -10,12 +12,21 @@ public class EventSerializer implements NetSerializer {
 
     @Override
     public void write(ByteBuffer buffer, Object object) {
-        writeString(buffer, object.getClass().getName());
-        writeString(buffer, JsonIO.write(object));
+        if (object instanceof FrameworkMessage message) {
+            buffer.put((byte) 0);
+            writeFramework(buffer, message);
+        } else {
+            buffer.put((byte) 1);
+            writeString(buffer, object.getClass().getName());
+            writeString(buffer, JsonIO.write(object));
+        }
     }
 
     @Override
     public Object read(ByteBuffer buffer) {
+        byte id = buffer.get();
+        if (id == 0) return readFramework(buffer);
+
         String className = readString(buffer);
         try {
             Class<?> type = Class.forName(className);
@@ -24,6 +35,27 @@ public class EventSerializer implements NetSerializer {
             Log.err("Couldn`t find the event class received from another server. Class name is @", className, ignored);
             return null;
         }
+    }
+
+    public void writeFramework(ByteBuffer buffer, FrameworkMessage message) {
+        if (message instanceof Ping || message instanceof DiscoverHost) buffer.put((byte) 0); // unused
+        else if (message instanceof RegisterTCP reg) buffer.put((byte) 1).putInt(reg.connectionID);
+        else if (message instanceof RegisterUDP reg) buffer.put((byte) 2).putInt(reg.connectionID);
+        else if (message instanceof KeepAlive) buffer.put((byte) 3);
+    }
+
+    public FrameworkMessage readFramework(ByteBuffer buffer) {
+        byte id = buffer.get();
+        if (id == 1)
+            return new RegisterTCP() {{
+                connectionID = buffer.getInt();
+            }};
+        if (id == 2)
+            return new RegisterUDP() {{
+                connectionID = buffer.getInt();
+            }};
+        if (id == 3) return FrameworkMessage.keepAlive;
+        throw new RuntimeException("Unknown framework message!");
     }
 
     public static void writeString(ByteBuffer buffer, String message) {
